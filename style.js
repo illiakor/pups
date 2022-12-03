3d539df40325b12edd307e1b6e0d525bf81ca6ca
illiakor <illiakk1807@gmail.com>
2022-12-03 23:34:52 +0000

    pups

---
+++ style.js
@@ -0,0 +1,7 @@
+alert("Привіт,зараз я дізнаююся з якого ти міста!")
+alert("Зараз розділи назву смого міста на 2 частини і напиши мені по черзі ок?")
+
+var firstNumber = prompt('ПЕРША ЧАСТИНА')
+var secondNumber = prompt('ДРУГА ЧАСТИНА')
+alert(firstNumber+secondNumber)
+alert("Я вже їду друже))))")
